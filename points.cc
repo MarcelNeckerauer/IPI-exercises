220d75a4f5c22e7d42ce6b9f6e17e0e316ecc161
Marcel Neckerauer <marcel.neckerauer@web.de>
2017-12-12 15:27:30 +0100

    points.cc angefangen

--- points.cc
+++ points.cc
@@ -7,16 +7,63 @@ private:
   double x, y;
   
 public:
-  Point2D();
-  Point2D(double xNew, double yNew);
+  Point2D() {
+	  x = 0;
+	  y = 0;
+  }
+  Point2D(double xNew, double yNew) {
+	  x = xNew;
+	  y = yNew;
+  }
 
-  void setX(double xNew);
-  void setY(double yNew);
+  void setX(double xNew) {
+	  x = xNew;
+  }
+  
+  void setY(double yNew) {
+	  y = yNew;
+  }
 
-  double getX() const;
-  double getY() const;
+  double getX() const {
+	  return x;
+  }
+  double getY() const {
+	  return y;
+  }
 
-  double calcDistance(const Point2D& newPoint) const;
+  double calcDistance(const Point2D& newPoint) const {
+	  double newX = (this -> x + newPoint.getX()) * (this -> x + newPoint.getX());
+	  double newY = (this -> y + newPoint.getY()) * (this -> y + newPoint.getY());
+	  double tmp = newX + newY;
+	  double sol = sqrt(tmp);
+	  return sol;
+};
+
+class Point3D : Point2D {
+private:
+	double z;
+public:
+	Point3D() : Point2D() {
+		z = 0;
+	}
+	
+	Point3D(double xNew, double yNew, double zNew) : public Point2D(double xNew, double yNew), z(zNew) { }
+	void setZ (double zNew) {
+		z = zNew;
+	}
+	
+	double getZ() const {
+		return z;
+	}
+	
+	double calcDistance(const Point3D& newPoint) const {
+		double newX = (this -> x + newPoint.getX()) * (this -> x + newPoint.getX());
+		double newY = (this -> y + newPoint.getY()) * (this -> y + newPoint.getY());
+		double newZ = (this -> z + newPoint.getZ()) * (this -> z + newPoint.getZ());
+		double tmp = newX + newY + newZ;
+		double sol = sqrt(tmp);
+		return sol;
+	}
 };
 
 int main() {
